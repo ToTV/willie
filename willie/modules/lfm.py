a4ccfd5b74e9acae4abef3e4b72c467d16379c5a
toor-totv <toor@titansof.tv>
2015-06-02 12:48:02 -0600

    lfm update for totv.theme

--- willie/modules/lfm.py
+++ willie/modules/lfm.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, print_function, unicode_literals
 import json
 
 import requests
+from totv.theme import render_error, render, EntityGroup, Entity
 from willie.module import commands, example
 from willie import web
 
@@ -14,13 +15,14 @@ def configure(config):
 
 
 @commands('fm', 'last', 'lastfm', 'lfm', 'np')
-def lastfm(willie, trigger):
+def lastfm(bot, trigger):
     user = trigger.group(2)
-    apikey = str(willie.config.lastfm.apikey)
+    apikey = str(bot.config.lastfm.apikey)
     if not (user and user != ''):
-        user = willie.db.get_nick_value(trigger.nick, 'lastfm_user')
+        user = bot.db.get_nick_value(trigger.nick, 'lastfm_user')
         if not user:
-            willie.reply("Invalid username given or no username set. Use .fmset to set a username.")
+            bot.reply(render_error("Invalid username given or no username set. "
+                                   "Use {}fmset to set a username.".format(bot.config.core.prefix), "lfm"))
             return
     # username variable prepared for insertion into REST string
     quoted_user = web.quote(user)
@@ -28,7 +30,10 @@ def lastfm(willie, trigger):
     recent_page = web.get(
         "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=%s&api_key=%s&format=json" % (
             quoted_user, apikey))
-    recent_track = json.loads(recent_page)['recenttracks']['track'][0]
+    try:
+        recent_track = json.loads(recent_page)['recenttracks']['track'][0]
+    except KeyError:
+        return bot.say(render_error("Failed to fetch user data", "lastfm"))
     #artist and track name pulled from recent_track
     quoted_artist = web.quote(recent_track['artist']['#text'])
     quoted_track = web.quote(recent_track['name'])
@@ -36,7 +41,7 @@ def lastfm(willie, trigger):
     trackinfo = requests.get(
         "http://ws.audioscrobbler.com/2.0/?method=track.getInfo&artist=%s&track=%s&username=%s&api_key=%s&format=json" % (
             quoted_artist, quoted_track, quoted_user, apikey))
-    print(trackinfo.text)
+
     if 'track' in trackinfo.json():
         trackinfo = trackinfo.json()['track']
         try:
@@ -50,13 +55,19 @@ def lastfm(willie, trigger):
 
     try:
         if loved > 0:
-            willie.say('\x035' + u'\u2665' + '\x03 %s - %s - (%s plays)' % (
-                recent_track['artist']['#text'], recent_track['name'], playcount))
+            prefix = '\x035' + u'\u2665' + '\x03'
         else:
-            willie.say(
-                u'\u266A' + ' %s - %s (%s plays)' % (recent_track['artist']['#text'], recent_track['name'], playcount))
+            prefix = '\u266A'
+        bot.say(render(items=[
+            EntityGroup([Entity("LastFM")]),
+            EntityGroup([
+                Entity("{} {}".format(prefix, recent_track['artist']['#text'])),
+                Entity(recent_track['name'])
+            ]),
+            EntityGroup([Entity("Plays", playcount)])
+        ]))
     except KeyError:
-        willie.say("Couldn't find any recent tracks")
+        bot.say(render_error("Couldn't find any recent tracks", "lastfm"))
 
 
 @commands('fmset')
@@ -64,4 +75,7 @@ def lastfm(willie, trigger):
 def update_lastfm_user(bot, trigger):
     user = trigger.group(2)
     bot.db.set_nick_value(trigger.nick, 'lastfm_user', user)
-    bot.reply('Thanks, ' + user)
+    bot.say(render([
+        EntityGroup([Entity("LastFM")]),
+        EntityGroup([Entity("User set successfully")])
+    ]))
